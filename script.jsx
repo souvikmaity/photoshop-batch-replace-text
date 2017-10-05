68263f4f64afb767665ef9ee8aa86121d5cc243c
Souvik Maity <souvik@squadrun.co>
2017-10-06 04:49:48 +0530

    Adding script files

---
+++ script.jsx
@@ -0,0 +1,37 @@
+var doc;
+
+try {
+    doc = app.activeDocument;
+    // the front document
+} catch(e) {}
+var txtLayer = doc.layers.getByName('promo');
+// obviously, the text layer if found
+
+if (txtLayer) {
+  for (var j = 3; i < 26; i++) {
+    txtLayer.textItem.contents = '\'EarnWithSquad' + j + '\'';
+      // update layer content
+      var ext = '.png',
+      dir = decodeURI(doc.path) + '/png24',
+      fileName = dir + '/' + j + ext,
+      i = 0;
+      if (!Folder(dir).exists) Folder(dir).create();
+        while (File(fileName).exists)
+          fileName = dir + '/' + j + '-' + (++i) + ext;
+          var file = new File(fileName),
+          opts = new ExportOptionsSaveForWeb();
+          with (opts) {
+            format = SaveDocumentType.PNG;
+            PNG24 = true;
+          }
+          doc.exportDocument(file, ExportType.SAVEFORWEB, opts);
+          // save for web
+    }
+  }
+
+if (doc) {
+    doc.close(SaveOptions.DONOTSAVECHANGES);
+    // close the original layered document without saving
+}
+doc = null;
+// remove reference
